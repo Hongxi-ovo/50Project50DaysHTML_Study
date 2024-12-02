d0936994334c5d0974817ab5d1961186352a2442
虹希 <hongxiovo@gmail.com>
2024-12-02 21:43:02 +0800

    第三天

---
+++ 3days/script.js
@@ -0,0 +1,12 @@
+$(function () {
+  $(".icon-runtongyiyaoyihuifu_caidan-51").click(function () {
+    $(".icon").addClass("iconSelect");
+    $(".content").addClass("contentSelect");
+    $(".footClass").addClass("footSelect");
+  });
+  $(".icon-runtongyiyaoyihuifu_chacha-29").click(function () {
+    $(".icon").removeClass("iconSelect");
+    $(".content").removeClass("contentSelect");
+    $(".footClass").removeClass("footSelect");
+  });
+});
